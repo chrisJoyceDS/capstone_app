ac58c884ebb063d032415cdc2759418efdd971e6
Christopher Joyce <128563055+chrisJoyceDS@users.noreply.github.com>
2023-05-12 13:51:53 -0400

    Update user.py

--- capstone_app/user.py
+++ capstone_app/user.py
@@ -15,7 +15,7 @@ class User:
         self.access_token = None
 
     def authenticate(self):
-        
+
         # Scopes for the API access
         scope = 'user-library-read user-top-read playlist-read-private user-follow-read'
 
@@ -28,13 +28,19 @@ class User:
         # Generate the authorization URL
         auth_url = auth.get_authorize_url()
 
-        # Make a request to the authorization URL
-        response = requests.get(auth_url)
+        # Display the authorization URL to the user
+        st.write('Please visit the following URL and enter the code that you get when you authenticate:')
+        st.write(auth_url)
 
-        # Parse the response to get the access token
-        access_token = response.json()['access_token']
-        
+        # Get the code from the user
+        code = st.text_input('Enter the code: ')
+
+        # Get the access token
+        token_info = auth.get_access_token(code)
+        access_token = token_info['access_token']
+
         # Create a Spotify client
         sp = spotipy.Spotify(auth=access_token)
         self.access_token = sp
+
 
